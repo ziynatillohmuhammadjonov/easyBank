ac419e8c8acd84a70c6c4281002952c6053f8d6e
Ziynatilloh <ziynatillomuhammadjonov@gmail.com>
2023-01-03 15:42:15 +0500

    Navbar fixed

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 const toglerBtn = document.querySelector(".togler-open");
 const wrapper = document.querySelector(".wrapper");
 // const showwrapper = document.querySelector(".show-wrapper");
+const toggleBtnImg = document.querySelector(".togler-open__img");
 const cover = document.querySelector(".cover");
 
 toglerBtn.addEventListener("click", () => {
   wrapper.classList.toggle("show-wrapper");
   cover.classList.toggle("show-cover");
+  if (wrapper.classList.contains("show-wrapper")) {
+    toggleBtnImg.src = "./images/toggler-close.svg";
+  } else {
+    toggleBtnImg.src = "./images/toggler-open.svg";
+  }
 });
